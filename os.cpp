9a30fb07b1777d56cb536aeb0c59d91c2b122874
Ajit@021 <ajitkumars022@gmail.com>
2018-03-25 21:55:36 +0530

    input from the user and display

--- os.cpp
+++ os.cpp
@@ -1,2 +1,48 @@
 #include<stdio.h>
 
+#define time=4;
+void display(int,struct process);
+struct process 
+{
+	int pid;
+	int burst;
+	int arrival;
+	int priority;
+	int wait;
+	int start;
+	int remain;
+	
+};
+void display(int n,struct process l[])
+{
+  	printf("\n PROCESS\tPRIORITY\tBURST-TIME\tARRIVALTIME\n");
+	for(int i=0;i<n;i++)
+	{
+	  printf(" %d\t\t",l[i].pid);
+	  printf("%d\t\t",l[i].priority);
+	  printf("%d\t\t",l[i].burst);
+	  printf("%d\t\t",l[i].arrival);
+	  printf("\n");
+	  
+		
+	}
+}
+int main()
+{
+	int n;
+	printf("Enter the number of process:=");
+	scanf("%d",&n);
+	struct process l[n];
+    printf("\n PROCESS\tPRIORITY\tBURST-TIME\tARRIVALTIME\n");
+	for(int i=0;i<n;i++)
+	{
+	  scanf("%d",&l[i].pid);
+	  scanf("%d",&l[i].priority);
+	  scanf("%d",&l[i].burst);
+	  scanf("%d",&l[i].arrival);
+	  printf("\n");
+		
+	}
+	display(n,l);
+	
+}
